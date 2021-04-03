14ff155d4090991abb7e6c096356f35a3a7ebfe1
eveporcello <eve@moonhighway.com>
2021-04-03 14:27:54 -0700

    adds cat files

---
+++ index.js
@@ -0,0 +1,43 @@
+const { ApolloServer, gql } = require("apollo-server");
+const {
+  buildFederatedSchema
+} = require("@apollo/federation");
+const cats = require("./cats.json");
+
+const typeDefs = gql`
+  type Cat {
+    id: ID!
+    name: String!
+    weight: Float!
+    photo: Photo!
+  }
+
+  type Photo {
+    full: String!
+    thumb: String!
+  }
+
+  type Query {
+    allCats: [Cat!]!
+    catCount: Int!
+  }
+`;
+const resolvers = {
+  Query: {
+    allCats: () => cats,
+    catCount: () => cats.length
+  }
+};
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([
+    {
+      typeDefs,
+      resolvers
+    }
+  ])
+});
+
+server.listen(4001).then(({ url }) => {
+  console.log(`🐱 Cat Service running at ${url}`);
+});
